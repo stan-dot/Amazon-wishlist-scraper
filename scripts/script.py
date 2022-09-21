f82f1191f9e6262c989f226ee4d61b6d35dd3083
stan-dot <stanislawmalinowski09@gmail.com>
2022-09-21 20:59:59 +0200

    script runs and outputs a list of titles

--- scripts/script.py
+++ scripts/script.py
@@ -1,18 +1,30 @@
-# import the necessary libraries
-import nltk
-import cli
-import string
+import sys
 import re
-
-name = 'fiction'
-
-# read file into memory as a string
-
-# process the whole string 
-def process_string(s:str)-> dict:
-  return {}
-
-# save into output
-
+import click
+from bs4 import BeautifulSoup
 
 # launch different file depending on the argument
+name = 'fiction'
+
+
+# process the whole string 
+def process_string(s:str)->(list[str]):
+  output_list: list[str] = []
+  soup = BeautifulSoup(s, 'html.parser')
+  links_list = soup.find_all('a', href=True, title=True)
+  for item in links_list:
+    test_print = item.string
+    if(test_print != None):
+      output_list.append(test_print.strip())
+  return output_list
+
+
+
+
+# read file into memory as a string
+with open(f'data/{name}.html') as f:
+  contents:str = f.read()
+  titles:list[str]=process_string(contents)
+  print(titles)
+
+# save into output
